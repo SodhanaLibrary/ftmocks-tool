cec94ae20bace08b3fd74740c671d88315041d82
Srinivas Dasari <srinivas.dasari@nilesecure.com>
2025-01-01 13:40:45 +0530

    Snaps feature

--- src/components/recordedEvents/CodeUtils.js
+++ src/components/recordedEvents/CodeUtils.js
@@ -1,12 +1,12 @@
 const isSameRequest = (req1, req2) => {
   let matched = true;
-  if(req1.url !== req2.url) {
+  if (req1.url !== req2.url) {
     matched = false;
-  } else if(req1.method !== req2.method) {
+  } else if (req1.method !== req2.method) {
     matched = false;
   }
   return matched;
-}
+};
 
 function getLastWordFromApiUrl(apiUrl) {
   // Split the URL into parts by '/'
@@ -25,48 +25,60 @@ function getLastWordFromApiUrl(apiUrl) {
   return null;
 }
 
-
-export function processURL(url, ignoreParams=[]) {
+export function processURL(url, ignoreParams = []) {
   // Remove the hostname from the URL
   const urlWithoutHost = url.replace(/^(https?:\/\/)?[^\/]+/, '');
   const processedURL = new URL(`http://domain.com${urlWithoutHost}`);
   const params = new URLSearchParams(processedURL.search);
-  if(ignoreParams?.length > 0) {
-    ignoreParams.forEach(ip => {
+  if (ignoreParams?.length > 0) {
+    ignoreParams.forEach((ip) => {
       params.delete(ip);
     });
   }
   params.sort();
   return decodeURIComponent(`${processedURL.pathname}?${params}`);
-} 
+}
 
 export function compareMockToRequest(mock, req) {
   const mockURL = processURL(mock.url, mock.ignoreParams);
   const reqURL = processURL(req.originalUrl, mock.ignoreParams);
-  return isSameRequest({url: mockURL, method: mock.method}, {
-    method: req.method,
-    url: reqURL,
-  });
+  return isSameRequest(
+    { url: mockURL, method: mock.method },
+    {
+      method: req.method,
+      url: reqURL,
+    }
+  );
 }
 
 export function makeSubsetActions(actions, tests) {
   let currentActions = [];
   const testActions = {};
   actions.forEach((action) => {
-    if(['click', 'type', 'change', 'dblclick', 'contextmenu'].includes(action.type)) {
+    if (
+      ['click', 'type', 'change', 'dblclick', 'contextmenu'].includes(
+        action.type
+      )
+    ) {
       currentActions.push(action);
-    } else if(['POST', 'PUT', 'PATCH', 'DELETE'].includes(action.type)) {
-      const test = tests.find(test => test.mocks.find(mock => compareMockToRequest(mock, {
-        originalUrl: action.target,
-        method: action.type
-      })));
-      if(test) {
+    } else if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(action.type)) {
+      const test = tests.find((test) =>
+        test.mocks.find((mock) =>
+          compareMockToRequest(mock, {
+            originalUrl: action.target,
+            method: action.type,
+          })
+        )
+      );
+      if (test) {
         if (!testActions[test.name]?.actions) {
           testActions[test.name] = {
-            actions: []
-          }
+            actions: [],
+          };
         }
-        testActions[test.name].actions = testActions[test.name].actions.concat(...currentActions);   
+        testActions[test.name].actions = testActions[test.name].actions.concat(
+          ...currentActions
+        );
       } else {
         let testType = 'create';
         if (action.type === 'POST') {
@@ -81,18 +93,20 @@ export function makeSubsetActions(actions, tests) {
         const lastWord = getLastWordFromApiUrl(action.target);
         if (!testActions[`${testType} ${lastWord}`]?.actions) {
           testActions[`${testType} ${lastWord}`] = {
-            actions: []
+            actions: [],
           };
         }
-        testActions[`${testType} ${lastWord}`].actions = testActions[`${testType} ${lastWord}`].actions.concat(...currentActions);   
+        testActions[`${testType} ${lastWord}`].actions = testActions[
+          `${testType} ${lastWord}`
+        ].actions.concat(...currentActions);
       }
       currentActions = [];
     }
   });
-  if(currentActions.length > 0) {
+  if (currentActions.length > 0) {
     if (!testActions[`ftmocks basic render`]?.actions) {
       testActions[`ftmocks basic render`] = {
-        actions: currentActions
+        actions: currentActions,
       };
     }
   }
@@ -100,79 +114,85 @@ export function makeSubsetActions(actions, tests) {
 }
 
 export function generateRTLCode(actions, tests = []) {
-    const testActions = makeSubsetActions(actions, tests);
-    const testCodes = [];
-    Object.keys(testActions).forEach(testName => {
-      let testCode = testActions[testName].actions.map((action) => {
+  const testActions = makeSubsetActions(actions, tests);
+  const testCodes = [];
+  Object.keys(testActions).forEach((testName) => {
+    let testCode = testActions[testName].actions
+      .map((action) => {
         switch (action.type) {
-          case "click":
+          case 'click':
             return [
               ``,
               `  await waitFor(() => {expect(getByXPath(dom.container, "${action.target}")).toBeInTheDocument();});`,
-              `  fireEvent.click(getByXPath(dom.container, "${action.target}"))`
+              `  fireEvent.click(getByXPath(dom.container, "${action.target}"))`,
             ].join('\n');
-          case "type":
+          case 'type':
             return `  fireEvent.type(getByXPath(dom.container, "${action.target}"), "${action.value}");`;
-          case "change":
+          case 'change':
             return `  fireEvent.change(getByXPath(dom.container, "${action.target}"), "${action.value}");`;
-          case "dblclick":
+          case 'dblclick':
             return `  fireEvent.dblClick(getByXPath(dom.container, "${action.target}"));`;
-          case "contextmenu":
+          case 'contextmenu':
             return `  fireEvent.contextMenu(getByXPath(dom.container, "${action.target}"));`;
-          case "POST":
+          case 'POST':
             return `-------------------------------------------------------------`;
-          case "PUT":
+          case 'PUT':
             return `-------------------------------------------------------------`;
-          case "PATCH":
+          case 'PATCH':
             return `-------------------------------------------------------------`;
-          case "DELETE":
+          case 'DELETE':
             return `-------------------------------------------------------------`;
           default:
             return null;
         }
-      }).filter(cd => !!cd).join("\n");
-      testCode = [
-        `// ${testName} test case`,
-        `it('${testName}', async () => {`,
-        `  await initiateJestFetch(jest, ftmocksConifg, '${testName}');`,
-        `  const dom = render(<App />);`,
-        testCode,
-        `});`,
-        ``
-      ].join("\n");
-      testCodes.push(testCode);
-    });
-    return testCodes.join("\n");
-};
+      })
+      .filter((cd) => !!cd)
+      .join('\n');
+    testCode = [
+      `// ${testName} test case`,
+      `it('${testName}', async () => {`,
+      `  await initiateJestFetch(jest, ftmocksConifg, '${testName}');`,
+      `  const dom = render(<App />);`,
+      testCode,
+      `});`,
+      ``,
+    ].join('\n');
+    testCodes.push(testCode);
+  });
+  return testCodes.join('\n');
+}
 
 export function generatePlaywrightCode(actions) {
-  return actions.map((action) => {
+  return actions
+    .map((action) => {
       switch (action.type) {
-          case "click":
-              return [
-                  ``,
-                  `await expect(page.locator('${action.target}')).toBeVisible();`,
-                  `await page.click('${action.target}');`
-              ].join('\n');
-          case "type":
-              return `await page.fill('${action.target}', '${action.value}');`;
-          case "change":
-              // Assuming change event can be simulated via filling or selecting a value
-              return `await page.fill('${action.target}', '${action.value}');`;
-          case "dblclick":
-              return `await page.dblclick('${action.target}');`;
-          case "contextmenu":
-              return `await page.click('${action.target}', { button: 'right' });`;
-          case "POST":
-              return `-------------------------------------------------------------`;
-          case "PUT":
-              return `-------------------------------------------------------------`;
-          case "PATCH":
-              return `-------------------------------------------------------------`;
-          case "DELETE":
-              return `-------------------------------------------------------------`;
-          default:
-              return null;
+        case 'click':
+          return [
+            ``,
+            `await expect(page.locator('${action.target}')).toBeVisible();`,
+            `await page.click('${action.target}');`,
+          ].join('\n');
+        case 'type':
+          return `await page.fill('${action.target}', '${action.value}');`;
+        case 'change':
+          // Assuming change event can be simulated via filling or selecting a value
+          return `await page.fill('${action.target}', '${action.value}');`;
+        case 'dblclick':
+          return `await page.dblclick('${action.target}');`;
+        case 'contextmenu':
+          return `await page.click('${action.target}', { button: 'right' });`;
+        case 'POST':
+          return `-------------------------------------------------------------`;
+        case 'PUT':
+          return `-------------------------------------------------------------`;
+        case 'PATCH':
+          return `-------------------------------------------------------------`;
+        case 'DELETE':
+          return `-------------------------------------------------------------`;
+        default:
+          return null;
       }
-  }).filter(cd => !!cd).join("\n");
-};
+    })
+    .filter((cd) => !!cd)
+    .join('\n');
+}
